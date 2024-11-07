665523b0b80e21ee7e60ceb8c41fd4fc32994f7e
kartik7974 <Kartik@1403>
2024-11-07 18:07:39 +0530

    Add health check endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'signup', to: 'accounts#create'
   post 'login', to: 'logins#login'
+  get '/healthz', to: 'health#check'
 end
