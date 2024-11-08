7daf80d70a77b861af1f73e7204ce57afbb98b0f
kartik7974 <Kartik@1403>
2024-11-08 16:04:24 +0530

    Add babel plugin for private methods

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function(api) {
   var validEnv = ['development', 'test', 'production']
   var currentEnv = api.env()
+  var isDevelopmentEnv = api.env('development')
+  var isProductionEnv = api.env('production')
+  var isTestEnv = api.env('test')
 
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
@@ -14,14 +17,33 @@ module.exports = function(api) {
 
   return {
     presets: [
-      [
+      isTestEnv && [
         '@babel/preset-env',
         {
           targets: {
             node: 'current'
           }
         }
+      ],
+      (isProductionEnv || isDevelopmentEnv) && [
+        '@babel/preset-env',
+        {
+          forceAllTransforms: true,
+          useBuiltIns: 'entry',
+          corejs: 3,
+          modules: false,
+          exclude: ['transform-typeof-symbol']
+        }
       ]
-    ]
+    ].filter(Boolean),
+    plugins: [
+      '@babel/plugin-transform-destructuring',
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+      ['@babel/plugin-transform-runtime', { helpers: false }],
+      '@babel/plugin-transform-regenerator',
+      isProductionEnv && ['babel-plugin-transform-react-remove-prop-types', { removeImport: true }]
+    ].filter(Boolean)
   }
 }
